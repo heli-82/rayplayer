45178567082b158a0c13a70ffbaadbd555104b11
heli82 <89199260267den@gmail.com>
2024-11-04 17:40:20 +0500

    nfd instead of zenity

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <cstdio>
+#include <nfd.h>
+#include <queue>
 #include <raylib.h>
 #include <stdlib.h>
 #include <string>
@@ -15,21 +17,58 @@ const Color BG_COLOR = Color{0x14, 0x14, 0x1b, 0xff};
 const std::pair<Color, Color> PALETTE = {Color{0xb1, 0x93, 0xba, 0xff},
                                          Color{0x84, 0x66, 0x8e, 0xff}};
 
-struct MetaData {
+struct Track {
+    std::string path;
     std::string title;
     std::string artist;
 };
 
-std::string get_path() {
-    char path[1024];
-    FILE *f = popen("zenity --file-selection --file-filter=*.mp3", "r");
-    std::fgets(path, 1024, f);
-    std::string strpath = path;
-    return strpath.substr(0, strpath.length() - 1);
+std::queue<Track> Playlist;
+
+std::string get_filepath() {
+    NFD_Init();
+    std::string res = "";
+
+    nfdu8char_t *outPath;
+    nfdu8filteritem_t filters[1] = {
+        {"Audio file", "mp3, ogg, wav, aac, flac, m4a"}};
+    nfdopendialogu8args_t args = {0};
+    args.filterList = filters;
+    args.filterCount = 1;
+    nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &args);
+    switch (result) {
+    case NFD_OKAY: {
+        res = outPath;
+    } break;
+    default:
+        break;
+    }
+    return res;
 }
 
-MetaData get_meta(std::string path) {
-    MetaData result = MetaData{"", ""};
+std::vector<std::string> split_string(std::string &str, int maxlen,
+                                      char delimiter = ' ') {
+    int len = str.length();
+    str = (str.length() > 75) ? str.substr(0, 75) + "..." : str;
+    std::vector<std::string> result;
+    int current = 0;
+    while (current < str.length()) {
+        int predicted = std::min(len - 1, current + maxlen - 1);
+        while (str[predicted] != delimiter && predicted < len - 1 &&
+               predicted > current) {
+            predicted--;
+        }
+        if (predicted == current)
+            predicted = current + maxlen - 1;
+        result.push_back(str.substr(current, predicted - current + 1));
+        current = predicted + 1;
+    }
+    return result;
+}
+
+Track get_track() {
+    std::string path = get_filepath();
+    Track result = Track{path, "", ""};
 
     TagLib::FileRef f(path.data());
 
@@ -42,38 +81,6 @@ MetaData get_meta(std::string path) {
     return result;
 }
 
-std::vector<std::string> split_string(std::string &str, int maxlen,
-                                      char delimiter = ' ') {
-    int len = str.length();
-    str = (str.length() > 75) ? str.substr(0, 75) + "..." : str;
-    std::vector<std::string> result;
-    int current = 0;
-    while (current < str.length()) {
-        int predicted = std::min(len - 1, current + maxlen - 1);
-        while (str[predicted] != delimiter && predicted < len - 1 &&
-               predicted > 0) {
-            predicted--;
-        }
-        result.push_back(str.substr(current, predicted - current + 1));
-        current = predicted + 1;
-    }
-    return result;
-}
-
-struct Track {
-    std::string path;
-    std::vector<std::string> title;
-    std::vector<std::string> author;
-};
-
-Track preparation() {
-    std::string path = get_path();
-    MetaData data = get_meta(path);
-    std::vector<std::string> title = split_string(data.title, 25, ' ');
-    std::vector<std::string> author = split_string(data.artist, 30, ' ');
-    return Track{path, title, author};
-}
-
 int main() {
     double volume = 0.1;
     bool is_paused = false;
@@ -81,7 +88,9 @@ int main() {
     InitWindow(WIDTH, HEIGHT, "ray");
     InitAudioDevice();
 
-    Track track = preparation();
+    Track track = get_track();
+    std::vector<std::string> title = split_string(track.title, 25, ' ');
+    std::vector<std::string> author = split_string(track.artist, 30, ' ');
     Music music;
     music = LoadMusicStream(track.path.data());
 
@@ -91,7 +100,10 @@ int main() {
         UpdateMusicStream(music);
 
         if (IsKeyPressed(KEY_O)) {
-            track = preparation();
+            track = get_track();
+            title = split_string(track.title, 25, ' ');
+            author = split_string(track.artist, 30, ' ');
+
             UnloadMusicStream(music);
             music = LoadMusicStream(track.path.data());
             SetMusicVolume(music, 0.1);
@@ -119,7 +131,7 @@ int main() {
 
         // track title line
         int title_shift = 0;
-        for (std::string line : track.title) {
+        for (std::string line : title) {
             DrawText(line.data(), 10, 10 + 20 * title_shift, 20,
                      (!is_paused) ? PALETTE.first : LIGHTGRAY);
             title_shift++;
@@ -127,7 +139,7 @@ int main() {
 
         // author line
         int author_shift = 0;
-        for (std::string line : track.author) {
+        for (std::string line : author) {
             DrawText(line.data(), 10, 10 + 20 * title_shift + 10 * author_shift,
                      10, (!is_paused) ? PALETTE.second : GRAY);
             author_shift++;
@@ -140,9 +152,11 @@ int main() {
         const int MAXW = 15;
         int pos_x = WIDTH - 10 - MAXW;
         // volume slider background
-        DrawRectangle(pos_x, HEIGHT - 10 - MAXH, MAXW, MAXH, (!is_paused) ? PALETTE.second : GRAY);
+        DrawRectangle(pos_x, HEIGHT - 10 - MAXH, MAXW, MAXH,
+                      (!is_paused) ? PALETTE.second : GRAY);
         // slider itself
-        DrawRectangle(pos_x, pos_y, MAXW, h, (!is_paused) ? PALETTE.first : LIGHTGRAY);
+        DrawRectangle(pos_x, pos_y, MAXW, h,
+                      (!is_paused) ? PALETTE.first : LIGHTGRAY);
 
         EndDrawing();
     }
